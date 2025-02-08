481f9abd30918383a86fc99cf2312d752634adde
Dr Dilip Kumar <40090970+dilipnitjsr@users.noreply.github.com>
2025-02-08 16:13:11 +0000

    hi

--- Code/test3.py
+++ Code/test3.py
@@ -6,7 +6,20 @@ np.random.seed(42)
 date_range = pd.date_range(start='2025-01-01 9:15:00', periods=100, freq='H')
 print(date_range)
 open_price = np.random.uniform(100,200,size=100)
+high_price = open_price+ np.random.uniform(0,10,size=100)
 
+low_price = open_price- np.random.uniform(0,10,size=100)
+
+close_price =  np.random.uniform(low_price,high_price,size=100)
+df = DataFrame ({
+
+'date' : date_range,
+'open' : open_price,
+'high' : high_price,
+'low': low_price,
+'close' : close_price
+
+})
 x={"close": [100, 102, 101, 103, 105],"open": [100, 102, 101, 103, 105],"high": [100, 102, 101, 103, 105],"low": [100, 102, 101, 103, 105]}
 
 df = pd.DataFrame(x)
