6a334fa4ef1e979c2a04a386aef16f7016d2bea8
Dr Dilip Kumar <40090970+dilipnitjsr@users.noreply.github.com>
2025-02-08 16:22:26 +0000

    vu

--- Code/test3.py
+++ Code/test3.py
@@ -9,7 +9,7 @@ high_price = open_price+ np.random.uniform(0,10,size=100)
 
 low_price = open_price- np.random.uniform(0,10,size=100)
 
-close_price =  np.random.uniform(low_price,high_price,size=100)
+close_price =  np.random.uniform(low_price,high_price)
 df = pd.DataFrame ({
 'date' : date_range,
 'open' : open_price,
