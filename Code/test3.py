900e133976db961489cdb5a2fd50b1c5bd9d7446
Dr Dilip Kumar <40090970+dilipnitjsr@users.noreply.github.com>
2025-02-08 16:16:49 +0000

    hi

--- Code/test3.py
+++ Code/test3.py
@@ -1,6 +1,5 @@
 from finta import TA
 import pandas as pd
-
 import numpy as np
 np.random.seed(42)
 date_range = pd.date_range(start='2025-01-01 9:15:00', periods=100, freq='H')
@@ -11,15 +10,13 @@ high_price = open_price+ np.random.uniform(0,10,size=100)
 low_price = open_price- np.random.uniform(0,10,size=100)
 
 close_price =  np.random.uniform(low_price,high_price,size=100)
-df = DataFrame ({
+df = pd.DataFrame ({
 
 'date' : date_range,
 'open' : open_price,
 'high' : high_price,
 'low': low_price,
-'close' : close_price
-
-})
+'close' : close_price,})
 x={"close": [100, 102, 101, 103, 105],"open": [100, 102, 101, 103, 105],"high": [100, 102, 101, 103, 105],"low": [100, 102, 101, 103, 105]}
 
 df = pd.DataFrame(x)
